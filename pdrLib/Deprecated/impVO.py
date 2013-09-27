d3b71976322596e0687e0e94bc1139160501fdf6
José R. Sánchez-Gallego <j.sanchezgallego@uky.edu>
2013-09-27 17:04:34 -0400

    Revert "TRacking some files"
    This reverts commit 4826bd447d26c47ef985d9d63244c341492d1e6e.

--- pdrLib/Deprecated/impVO.py
+++
@@ -1,9 +0,0 @@
-try:
-    from astropy.io.votable.tree import VOTableFile, Resource, Table, Field
-    voModule = True
-except:
-    try:
-        from vo.tree import VOTableFile, Resource, Table, Field
-        voModule = True
-    except:
-        voModule = False
